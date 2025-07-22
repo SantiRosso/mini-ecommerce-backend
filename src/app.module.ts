993a878e82e841a782eb75eb47a68d560b8cb512
Santiago Rosso <santiagorosso@live.com.ar>
2025-07-21 23:53:27 -0300

    Set user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,34 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './modules/user/infrastructure/entities/user.typeorm.entity';
+import { UsersModule } from './modules/user/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST', 'localhost'),
+        port: parseInt(configService.get('DB_PORT', '3306')),
+        username: configService.get('DB_USERNAME', 'root'),
+        password: configService.get('DB_PASSWORD', ''),
+        database: configService.get('DB_NAME', 'mini_ecommerce'),
+        entities: [UserEntity],
+        synchronize: configService.get('NODE_ENV') !== 'production',
+        logging: configService.get('NODE_ENV') === 'development',
+        timezone: 'Z',
+        charset: 'utf8mb4',
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/modules/user/users.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserService } from './application/services/user.service';
+import { USER_REPOSITORY_TOKEN } from './domain/repositories/user.repository.token';
+import { UserEntity } from './infrastructure/entities/user.typeorm.entity';
+import { UserTypeOrmRepository } from './infrastructure/repositories/user.typeorm.repository';
+import { UserController } from './presentation/controllers/user.controller';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
+  controllers: [UserController],
+  providers: [
+    UserService,
+    {
+      provide: USER_REPOSITORY_TOKEN,
+      useClass: UserTypeOrmRepository,
+    },
+  ],
+  exports: [UserService],
+})
+export class UsersModule {}
