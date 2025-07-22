f701844ddd111747dbca59c34d94307d64a26952
Santiago Rosso <santiagorosso@live.com.ar>
2025-07-21 23:52:12 -0300

    Add user dto

---
+++ src/types/decorators.d.ts
@@ -0,0 +1,8 @@
+// Types for class-validator decorators
+declare module 'class-validator' {
+  export function IsEmail(): PropertyDecorator;
+  export function IsString(): PropertyDecorator;
+  export function IsNotEmpty(): PropertyDecorator;
+  export function IsOptional(): PropertyDecorator;
+  export function MinLength(min: number): PropertyDecorator;
+}
