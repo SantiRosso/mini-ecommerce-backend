e9bc3a1daf2582d2f32076372683bd7c028a7cfc
Santiago Rosso <santiagorosso@live.com.ar>
2025-07-24 19:51:15 -0300

    Add auth controller

---
+++ src/modules/auth/presentation/controllers/auth.controller.ts
@@ -0,0 +1,58 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Post,
+  Request,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthService } from '../../application/services/auth.service';
+import { LoginDto } from '../../application/dtos/login.dto';
+import { RegisterDto } from '../../application/dtos/register.dto';
+import { JwtAuthGuard } from '../../infrastructure/guards/jwt-auth.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async register(@Body(ValidationPipe) registerDto: RegisterDto) {
+    try {
+      return await this.authService.register(registerDto);
+    } catch (error) {
+      const errorMessage = (error as Error).message;
+      if (errorMessage === 'User with this email already exists') {
+        throw new HttpException(errorMessage, HttpStatus.CONFLICT);
+      }
+      throw new HttpException(
+        'Error creating user',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  @Post('login')
+  async login(@Body(ValidationPipe) loginDto: LoginDto) {
+    try {
+      return await this.authService.login(loginDto);
+    } catch (error) {
+      const errorMessage = (error as Error).message;
+      if (errorMessage === 'Invalid credentials') {
+        throw new HttpException(errorMessage, HttpStatus.UNAUTHORIZED);
+      }
+      throw new HttpException(
+        'Error during login',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  @Get('profile')
+  @UseGuards(JwtAuthGuard)
+  getProfile(@Request() req: { user: any }): any {
+    return req.user;
+  }
+}
